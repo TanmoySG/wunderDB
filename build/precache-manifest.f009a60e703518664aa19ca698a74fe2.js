63064698f15c1418ee82a40ae286ccb212534998
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2020-08-25 21:48:25 +0530

    Add files via upload

---
+++ build/precache-manifest.f009a60e703518664aa19ca698a74fe2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8009a8e67f879dbb618432ae78f420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "247a911362dd7a40a765",
+    "url": "/static/css/2.30d9feaa.chunk.css"
+  },
+  {
+    "revision": "0a973f1f453ccab58dc1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "247a911362dd7a40a765",
+    "url": "/static/js/2.cd9f6655.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.cd9f6655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a973f1f453ccab58dc1",
+    "url": "/static/js/main.1dc6179d.chunk.js"
+  },
+  {
+    "revision": "93a9c89d134900b882c9",
+    "url": "/static/js/runtime-main.023a59db.js"
+  },
+  {
+    "revision": "fd2dfd037249c4eb738f0e7d9321f706",
+    "url": "/static/media/wdb-complete.fd2dfd03.png"
+  },
+  {
+    "revision": "b252aa84ca771f024122381cddee98ca",
+    "url": "/static/media/wdb.b252aa84.png"
+  }
+]);
