400beac07d39887f487268e0c43b38750ffb2ae7
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-11-13 01:32:31 +0530

    Added Model

--- model/models.go
+++ model/models.go
@@ -3,15 +3,49 @@ package model
 type FilePath string
 type Identifier string
 
+type Paths map[string]FilePath
+
+// type Metadata struct {
+// 	Alias string     `json:"alias"`
+// 	ID    Identifier `json:"id"`
+// }
+
+type Metadata map[string]interface{}
+type User map[string]interface{}
+
 type WDBFileSystem struct {
-	Namespaces map[Identifier]Namespace
-	Users      map[Identifier]User
+	Namespaces map[Identifier]Namespace `json:"namespaces"`
+	Users      map[Identifier]User      `json:"users"`
 }
 
 type Namespace struct {
-	NamespaceAlias string
-	NamespaceID    Identifier
-	NamespacePath  FilePath
+	Databases map[Identifier]Database `json:"databases"`
+	Metadata  Metadata                `json:"metadata"`
+	Access    *Access                 `json:"access,omitempty"` // Use only namespace level access control for Initial build v2
+	Path      Paths                   `json:"path"`
 }
 
-type User map[string]interface{}
+type Database struct {
+	Collections map[Identifier]Collection `json:"collections"`
+	Metadata    Metadata                  `json:"metadata"`
+	Access      *Access                   `json:"access,omitempty"` // not in scope for Initial Version of 2.0
+	Path        Paths                     `json:"path"`
+}
+
+type Collection struct {
+	Data     Data     `json:"data"`
+	Metadata Metadata `json:"metadata"`
+	Schema   Schema   `json:"schema"`
+	Access   *Access  `json:"access,omitempty"` // not in scope for Initial Version of 2.0
+	Path     Paths    `json:"path"`
+}
+
+type Data map[Identifier]interface{}
+type Schema map[string]interface{}
+
+// Need to Decide exact requirements for Access
+// Access Control List
+type Access struct {
+	UserID         string
+	AllowedActions []string
+}
