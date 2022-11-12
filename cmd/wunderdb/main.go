ffb3bcabaa05b003bf8e3684764f18ab7a3dd41a
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-11-13 01:32:46 +0530

    Updated main

--- cmd/wunderdb/main.go
+++ cmd/wunderdb/main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+
+	"github.com/TanmoySG/wunderDB/model"
 )
 
 func main() {
-	fmt.Printf("123")
+
+	db := model.WDBFileSystem{
+		Namespaces: map[model.Identifier]model.Namespace{
+			"gns": {
+				Metadata: model.Metadata{},
+				Path: model.Paths{},
+				Databases: map[model.Identifier]model.Database{
+					"db-one": {
+						Metadata: model.Metadata{},
+						Path:     model.Paths{},
+						Collections: map[model.Identifier]model.Collection{
+							"coll-one": {
+								Path:     model.Paths{},
+								Metadata: model.Metadata{},
+								Data:     model.Data{},
+								Schema:   model.Schema{},
+							},
+						},
+					},
+				},
+			},
+		},
+		Users: map[model.Identifier]model.User{},
+	}
+
+	// jsonString, err := json.Marshal(db.Namespaces["gns"].Databases["db-one"].Collections["coll-one"])
+	jsonString, err := json.Marshal(db)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(jsonString))
 }
