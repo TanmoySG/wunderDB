591a7526473223974728042cff24b4d2a995479d
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-11-18 01:49:45 +0530

    Updated Model

--- model/models.go
+++ model/models.go
@@ -1,19 +1,24 @@
 package model
 
-type FilePath string
 type Identifier string
-
-type Paths map[string]*FilePath
 
 // type Metadata struct {
 // 	Alias string     `json:"alias"`
 // 	ID    Identifier `json:"id"`
 // }
 
-type Metadata map[string]interface{}
-type User map[string]interface{}
+type ExtraFields map[string]interface{}
 
-type WDBFileSystem struct {
+type Metadata map[string]interface{}
+type Authentication map[string]interface{}
+
+type User struct {
+	UserID         Identifier
+	Authentication Authentication
+	Metadata       Metadata
+}
+
+type WDB struct {
 	Namespaces map[Identifier]*Namespace `json:"namespaces"`
 	Users      map[Identifier]*User      `json:"users"`
 }
@@ -21,23 +26,20 @@ type WDBFileSystem struct {
 type Namespace struct {
 	Databases map[Identifier]*Database `json:"databases"`
 	Metadata  Metadata                 `json:"metadata"`
-	Access    Access                   `json:"access,omitempty"` // Use only namespace level access control for Initial build v2
-	Path      Paths                    `json:"path"`
+	Access    map[Identifier]*Access   `json:"access,omitempty"` // Use only namespace level access control for Initial build v2
 }
 
 type Database struct {
 	Collections map[Identifier]*Collection `json:"collections"`
 	Metadata    Metadata                   `json:"metadata"`
-	Access      Access                     `json:"access,omitempty"` // not in scope for Initial Version of 2.0
-	Path        Paths                      `json:"path"`
+	Access      map[Identifier]*Access     `json:"access,omitempty"` // not in scope for Initial Version of 2.0
 }
 
 type Collection struct {
-	Data     Data     `json:"data"`
-	Metadata Metadata `json:"metadata"`
-	Schema   Schema   `json:"schema"`
-	Access   Access   `json:"access,omitempty"` // not in scope for Initial Version of 2.0
-	Path     Paths    `json:"path"`
+	Data     Data                   `json:"data"`
+	Metadata Metadata               `json:"metadata"`
+	Schema   Schema                 `json:"schema"`
+	Access   map[Identifier]*Access `json:"access,omitempty"` // not in scope for Initial Version of 2.0
 }
 
 type Data map[Identifier]interface{}
