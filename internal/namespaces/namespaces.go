a243b113a8026949592ed86ab6445f3ed1dd3da3
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-11-19 10:36:58 +0530

    Updated Namespace pkg code

--- internal/namespaces/namespaces.go
+++ internal/namespaces/namespaces.go
@@ -3,14 +3,24 @@ package namespaces
 import (
 	"fmt"
 
+	er "github.com/TanmoySG/wunderDB/internal/errors"
 	"github.com/TanmoySG/wunderDB/model"
 )
 
 type Namespaces map[model.Identifier]*model.Namespace
 
+func (ns Namespaces) CheckIfNamespaceExists(namespaceID model.Identifier) bool {
+	_, nsExists := ns[namespaceID]
+	if nsExists {
+		return nsExistsInNamespaces
+	} else {
+		return nsDoesNotExistInNamespaces
+	}
+}
+
 func (ns Namespaces) CreateNewNamespace(namespaceID model.Identifier, metadata model.Metadata, access model.Access) error {
-	if ns.isNamespaceExists(namespaceID) {
-		return fmt.Errorf(NamespaceErrorFormat, NamespaceAlreadyExistsError.errCode, "error creating namespace", NamespaceAlreadyExistsError.errMessage)
+	if ns.CheckIfNamespaceExists(namespaceID) {
+		return fmt.Errorf(NamespaceErrorFormat, er.NamespaceAlreadyExistsError.ErrCode, "error creating namespace", er.NamespaceAlreadyExistsError.ErrMessage)
 	}
 	ns[namespaceID] = &model.Namespace{
 		Databases: map[model.Identifier]*model.Database{},
@@ -21,11 +31,11 @@ func (ns Namespaces) CreateNewNamespace(namespaceID model.Identifier, metadata m
 }
 
 func (ns Namespaces) DeleteNamespace(namespaceID model.Identifier) error {
-	if ns.isNamespaceExists(namespaceID) {
+	if ns.CheckIfNamespaceExists(namespaceID) {
 		delete(ns, namespaceID)
 		return nil
 	}
-	return fmt.Errorf(NamespaceErrorFormat, NamespaceDoesnotExistsError.errCode, "error deleting namespace", NamespaceDoesnotExistsError.errMessage)
+	return fmt.Errorf(NamespaceErrorFormat, er.NamespaceDoesNotExistsError.ErrCode, "error deleting namespace", er.NamespaceDoesNotExistsError.ErrMessage)
 }
 
 func (ns Namespaces) ModifyNamespaceMetadata(namespaceID model.Identifier) {
