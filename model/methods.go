0ff226455630c1ad784c8a2ec427bbf2d0cb0fcc
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-11-21 02:01:34 +0530

    Added NewWDBInstance method to initialize WDB object

--- model/methods.go
+++ model/methods.go
@@ -3,3 +3,11 @@ package model
 func (i Identifier) String() string {
 	return string(i)
 }
+
+func NewWDBInstance(namespaces map[Identifier]*Namespace, databases map[Identifier]*Database, users map[Identifier]*User) WDB {
+	return WDB{
+		Namespaces: namespaces,
+		Databases:  databases,
+		Users:      users,
+	}
+}
