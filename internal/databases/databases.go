d753e30085ca8c70431618873f65aad78d82fabc
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-11-29 11:16:31 +0530

    Updated internal/databases package methods

--- internal/databases/databases.go
+++ internal/databases/databases.go
@@ -1,19 +1,16 @@
 package databases
 
 import (
-	"fmt"
-
-	er "github.com/TanmoySG/wunderDB/internal/errors"
 	"github.com/TanmoySG/wunderDB/model"
 )
 
 type Databases map[model.Identifier]*model.Database
 
-func WithWDB(wdb model.WDB) Databases {
-	return wdb.Databases
+func WithWDB(databases Databases) Databases {
+	return databases
 }
 
-func (d Databases) CheckIfDatabaseExists(databaseID model.Identifier) bool {
+func (d Databases) CheckIfExists(databaseID model.Identifier) bool {
 	_, dbExists := d[databaseID]
 	if dbExists {
 		return databaseExists
@@ -22,29 +19,18 @@ func (d Databases) CheckIfDatabaseExists(databaseID model.Identifier) bool {
 	}
 }
 
-func (d Databases) CreateDatabase(databaseID model.Identifier, metadata model.Metadata, access model.Access) error {
-	if d.CheckIfDatabaseExists(databaseID) {
-		return fmt.Errorf(DatabaseErrorFormat, er.DatabaseAlreadyExistsError.ErrCode, "error creating database", er.DatabaseAlreadyExistsError.ErrMessage)
-	}
+func (d Databases) CreateDatabase(databaseID model.Identifier, metadata model.Metadata, access model.Access) {
 	d[databaseID] = &model.Database{
 		Collections: map[model.Identifier]*model.Collection{},
 		Metadata:    metadata,
 		Access:      map[model.Identifier]*model.Access{},
 	}
-	return nil
 }
 
-func (d Databases) GetDatabase(databaseID model.Identifier) (*model.Database, error) {
-	if !d.CheckIfDatabaseExists(databaseID) {
-		return nil, fmt.Errorf(DatabaseErrorFormat, er.DatabaseAlreadyExistsError.ErrCode, "error creating namespace", er.DatabaseAlreadyExistsError.ErrMessage)
-	}
-	return d[databaseID], nil
+func (d Databases) GetDatabase(databaseID model.Identifier) *model.Database {
+	return d[databaseID]
 }
 
-func (d Databases) DeleteDatabase(databaseID model.Identifier) error {
-	if d.CheckIfDatabaseExists(databaseID) {
-		delete(d, databaseID)
-		return nil
-	}
-	return fmt.Errorf(DatabaseErrorFormat, er.DatabaseDoesNotExistsError.ErrCode, "error deleting database", er.DatabaseDoesNotExistsError.ErrMessage)
+func (d Databases) DeleteDatabase(databaseID model.Identifier) {
+	delete(d, databaseID)
 }
