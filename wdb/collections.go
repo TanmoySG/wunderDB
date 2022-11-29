539f31cfb50dbfe2d2abe78d35b7ef099c78c25b
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-11-30 00:00:21 +0530

    Added collection Delete and Get methods

--- wdb/collections.go
+++ wdb/collections.go
@@ -8,7 +8,7 @@ import (
 	"github.com/TanmoySG/wunderDB/model"
 )
 
-func (wdb wdbClient) AddCollection(databaseId, collectionId model.Identifier, schema model.Schema, metadata model.Metadata) error {
+func (wdb wdbClient) AddCollection(databaseId, collectionId model.Identifier, schema model.Schema) error {
 	dbExists, database := wdb.Databases.CheckIfExists(databaseId)
 	if !dbExists {
 		return fmt.Errorf("error deleting database %s", er.DatabaseDoesNotExistsError.ErrMessage)
@@ -19,7 +19,38 @@ func (wdb wdbClient) AddCollection(databaseId, collectionId model.Identifier, sc
 	if exists, _ := collection.CheckIfExists(collectionId); exists {
 		return fmt.Errorf("error creating collection: %s", er.CollectionAlreadyExistsError.ErrMessage)
 	}
-	
+
 	collection.CreateCollection(collectionId, schema, model.Metadata{}, model.Access{})
+	return nil
+}
+
+func (wdb wdbClient) GetCollection(databaseId, collectionId model.Identifier) (*model.Collection, error) {
+	dbExists, database := wdb.Databases.CheckIfExists(databaseId)
+	if !dbExists {
+		return nil, fmt.Errorf("error fetching collection %s", er.DatabaseDoesNotExistsError.ErrMessage)
+	}
+
+	collection := c.UseDatabase(database)
+
+	if exists, _ := collection.CheckIfExists(collectionId); !exists {
+		return nil, fmt.Errorf("error fetching collection %s", er.CollectionAlreadyExistsError.ErrMessage)
+	}
+
+	return collection.GetCollection(collectionId), nil
+}
+
+func (wdb wdbClient) DeleteCollection(databaseId, collectionId model.Identifier) error {
+	dbExists, database := wdb.Databases.CheckIfExists(databaseId)
+	if !dbExists {
+		return fmt.Errorf("error deleting collection: %s", er.CollectionDoesNotExistsError.ErrMessage)
+	}
+
+	collection := c.UseDatabase(database)
+
+	if exists, _ := collection.CheckIfExists(collectionId); !exists {
+		return fmt.Errorf("error deleting collection %s", er.CollectionAlreadyExistsError.ErrMessage)
+	}
+
+	collection.DeleteCollection(collectionId)
 	return nil
 }
