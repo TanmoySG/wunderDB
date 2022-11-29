0a2d0ea8de61079b2df9ae1003bc1fe09d138b29
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-11-29 11:16:54 +0530

    Updated WDB main to use client

--- cmd/wunderdb/main.go
+++ cmd/wunderdb/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/TanmoySG/wunderDB/internal/databases"
+	"github.com/TanmoySG/wunderDB/internal/fsLoader"
+	"github.com/TanmoySG/wunderDB/model"
+	wdbClient "github.com/TanmoySG/wunderDB/wdb"
+)
 
 func main() {
-fmt.Printf("Hello World!")
+	fmt.Printf("Hello World!")
+
+	fs := fsLoader.NewWFileSystem("wfs/")
+
+	loadedDatabase, _ := fs.LoadDatabases()
+	db := databases.WithWDB(loadedDatabase)
+	wdbc := wdbClient.NewWdbClient(db)
+
+	err := wdbc.AddDatabase("freaked", model.Metadata{})
+	if err != nil {
+		fmt.Printf("err %s", err)
+	}
+
+	err = fs.UnloadDatabases(db)
+	if err != nil {
+		fmt.Printf("Err: %s", err)
+	}
 }
