7ae7d5083645c328cdaedecb63370785209be067
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-12-20 14:18:11 +0530

    Added role methods to wdbClient

--- pkg/wdb/wdb.go
+++ pkg/wdb/wdb.go
@@ -2,12 +2,15 @@ package wdbClient
 
 import (
 	d "github.com/TanmoySG/wunderDB/internal/databases"
+	r "github.com/TanmoySG/wunderDB/internal/roles"
+
 	er "github.com/TanmoySG/wunderDB/internal/errors"
 	"github.com/TanmoySG/wunderDB/model"
 )
 
 type wdbClient struct {
 	Databases d.Databases `json:"databases"`
+	Roles     r.Roles     `json:"roles"`
 }
 
 type Client interface {
@@ -26,10 +29,15 @@ type Client interface {
 	GetData(databaseId model.Identifier, collectionId model.Identifier, filters interface{}) (map[model.Identifier]*model.Datum, *er.WdbError)
 	UpdateData(databaseId model.Identifier, collectionId model.Identifier, updatedData interface{}, filters interface{}) *er.WdbError
 	DeleteData(databaseId model.Identifier, collectionId model.Identifier, filters interface{}) *er.WdbError
+
+	// Methods for Roles and Users
+	CreateRole(roleID model.Identifier, allowedActions []string, deniedActions []string) *er.WdbError
+	ListRole() r.Roles
 }
 
-func NewWdbClient(databases d.Databases) Client {
+func NewWdbClient(databases d.Databases, roles r.Roles) Client {
 	return wdbClient{
 		Databases: databases,
+		Roles:     roles,
 	}
 }
