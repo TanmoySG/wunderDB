54378f50db6216d3687076ecaf5e805c78e1ee6b
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2023-01-21 01:07:37 +0530

    Added CLI Code

--- cmd/wdbcli/cli.go
+++ cmd/wdbcli/cli.go
@@ -4,5 +4,5 @@ import "github.com/TanmoySG/wunderDB/internal/cli"
 
 func main() {
 	//  go run ./cmd/wdbcli/cli.go -start -p 8089 -o
-	cli.Listen()
+	cli.Run()
 }
--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,47 +1,31 @@
 package cli
 
 import (
-	"flag"
 	"os"
-	"strconv"
 
-	"github.com/TanmoySG/wunderDB/internal/config"
-	"github.com/TanmoySG/wunderDB/internal/server/lifecycle/shutdown"
-	"github.com/TanmoySG/wunderDB/internal/server/lifecycle/startup"
-	log "github.com/sirupsen/logrus"
+	"github.com/TanmoySG/wunderDB/internal/cli/cliHandlers"
+	"github.com/urfave/cli/v2"
 )
 
-func Listen() {
-	start := flag.Bool("start", false, "Starts wDB instance")
-	port := flag.String("p", "8086", "Port of wDB instance")
-	persistantStoragePath := flag.String("s", "", "Persistant FS of wDB instance")
-	override := flag.Bool("o", false, "Override Configurations for wDB instance")
+func Run() {
+	app := cli.NewApp()
+	app.Name = "wdbCLI"
+	app.Usage = "Command Line Interface for wdb"
 
-	flag.Parse()
-
-	if *start {
-		setEnvs(*port, *persistantStoragePath, *override)
-		startWdb()
-	}
-}
-
-func startWdb() {
-	c, err := config.Load()
-	if err != nil {
-		log.Fatalf("error loading configurations: %s", err)
-	}
-
-	w, err := startup.Prepare(*c)
-	if err != nil {
-		log.Fatalf("error starting wdb server: %s", err)
+	app.Commands = []*cli.Command{
+		{
+			Name:   "start",
+			Usage:  "starts the wdb instance",
+			Flags:  cliHandlers.StartOptFlags,
+			Action: cliHandlers.StartOptHandler,
+		},
+		{
+			Name:   "version",
+			Usage:  "version of CLI and wunderDb",
+			Action: cliHandlers.VersionOptHandler,
+		},
 	}
 
-	shutdown.Listen(*w, *c) // listens to shutdown signals
-	startup.Start(w, c)     // starts server and initial setup
-}
+	_ = app.Run(os.Args)
 
-func setEnvs(port, persistantStoragePath string, override bool) {
-	os.Setenv(config.PORT, port)
-	os.Setenv(config.PERSISTANT_STORAGE_PATH, persistantStoragePath)
-	os.Setenv(config.OVERRIDE_CONFIG, strconv.FormatBool(override))
 }
