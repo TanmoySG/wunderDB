8858b42cbba13ff0dcacebb584936bc28e8f6b32
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2023-01-22 01:28:21 +0530

    Renamed to wdb.go

--- cmd/wunderdb/wdb.go
+++ cmd/wunderdb/wdb.go
@@ -4,10 +4,17 @@ import (
 	"github.com/TanmoySG/wunderDB/internal/config"
 	"github.com/TanmoySG/wunderDB/internal/server/lifecycle/shutdown"
 	"github.com/TanmoySG/wunderDB/internal/server/lifecycle/startup"
+	"github.com/charmbracelet/lipgloss"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	var style = lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("#7D56F4")).PaddingTop(1)
+
+	log.Println(style.Render("Starting wunderDb..."))
+
 	c, err := config.Load()
 	if err != nil {
 		log.Fatalf("error loading configurations: %s", err)
