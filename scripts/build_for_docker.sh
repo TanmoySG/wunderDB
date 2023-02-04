d87e94aac3c7d507767c54a03717d504e549a0a5
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2023-02-04 22:14:05 +0530

    Fix docker build gh action

--- scripts/build_for_docker.sh
+++ scripts/build_for_docker.sh
@@ -7,6 +7,6 @@ env GOOS=linux \
     GOARCH=amd64 \
     go build -o ./bin/wdb_docker ./cmd/wunderdb/wdb.go
 
-docker build -t wunderdb .
+docker buildx build -t wunderdb .
 docker tag wunderdb:latest $IMAGE_NAME:$IMAGE_TAG
 docker push $IMAGE_NAME:$IMAGE_TAG
