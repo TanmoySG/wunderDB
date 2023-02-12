2be18a700de42ac864b556f73a2e752430fcbbf2
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2023-02-12 13:40:25 +0530

    Reset Repo and App Version to v1 #68

--- cmd/wdbctl/cli.go
+++ cmd/wdbctl/cli.go
@@ -3,8 +3,8 @@ package main
 import c "github.com/TanmoySG/wunderDB/internal/cli"
 
 // ALWAYS UPDATE WHILE PUBLISHING NEW VERSION
-const WDB_VERSION = "v2.0.0-beta"
-const CLI_VERSION = "v0.0.1-beta"
+const WDB_VERSION = "v1.0.0-rc.1"
+const CLI_VERSION = "v0.0.1-rc.1"
 
 func main() {
 	//  go run ./cmd/wdbctl/cli.go start -p 8089 -o
