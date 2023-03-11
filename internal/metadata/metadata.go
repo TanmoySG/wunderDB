d401456d6151dc5c69593758022f06a25a722ee5
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2023-03-11 12:31:40 +0530

    initial metadata code setup

---
+++ internal/metadata/metadata.go
@@ -0,0 +1,43 @@
+package metadata
+
+import (
+	"time"
+
+	"github.com/TanmoySG/wunderDB/model"
+)
+
+type metadata model.Metadata
+
+const (
+	CreatedAt = "created_at"
+	UpdatedAt = "updated_at"
+)
+
+// type changeTimeMetadata struct {
+// 	CreatedAt string `json:"created_at"`
+// 	UpdatedAt string `json:"updated_at"`
+// }
+
+func BasicCreateTimestampMetadata(optionalMetadata ...any) metadata {
+	currentTime := time.Now()
+	currentTimestamp := currentTime.Unix() // second precisioned timestamp
+
+	return metadata{
+		CreatedAt: currentTimestamp,
+		UpdatedAt: currentTimestamp,
+	}
+}
+
+func (m metadata) BasicUpdateTimestampMetadata(optionalMetadata ...any) metadata {
+	currentTime := time.Now()
+	currentTimestamp := currentTime.Unix() // second precisioned timestamp
+
+	return metadata{
+		CreatedAt: m[CreatedAt],
+		UpdatedAt: currentTimestamp,
+	}
+}
+
+func (m metadata) Commit() model.Metadata {
+	return model.Metadata(m)
+}
