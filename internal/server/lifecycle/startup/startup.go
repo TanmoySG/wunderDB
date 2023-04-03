a53ae0857eb28240dc90c7415e3d55ed3d7ea79c
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2023-04-04 02:02:31 +0530

    fixed filepath formats

--- internal/server/lifecycle/startup/startup.go
+++ internal/server/lifecycle/startup/startup.go
@@ -55,6 +55,6 @@ func Start(w *model.WDB, c *config.Config) {
 	wdbc := wdbClient.NewWdbClient(wdbClientConfigurations, w.Databases, w.Roles, w.Users, authentication.MD5)
 	wdbc.InitializeAdmin(c)
 
-	server := wdbs.NewWdbServer(wdbc, c.Port, c.RootDirectoryPath)
+	server := wdbs.NewWdbServer(wdbc, c.RootDirectoryPath, c.Port)
 	server.Start()
 }
--- internal/txlogs/constants.go
+++ internal/txlogs/constants.go
@@ -1,6 +1,6 @@
 package txlogs
 
-var WDB_DOT_TX_LOG_BASEPATH = "%s/txlogs/"
+var WDB_DOT_TX_LOG_BASEPATH = "%s/txlogs"
 var WDB_DOT_TX_LOG_FILEPATH = "%s/%s"
 
 const wdbDotTxLogFilename = "tx.log.json"
